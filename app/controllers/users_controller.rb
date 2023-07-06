1cf582866f795140975e52a0b75d144e2bf3b2d1
Arian <arianahmadi1@gmail.com>
2023-07-06 07:09:03 -0400

    fix the database migrate issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,25 @@ class UsersController < ApplicationController
   end
 
   def create
+
     @user = User.new(user_params)
     if @user.save
-      # Handle successful registration
-      redirect_to root_path, notice: "Registration successful!"
+      session[:user_id] = @user.id
+      redirect_to '/'
     else
-      # Handle registration failure
       render :new
+      #redirect_to '/signup'
     end
   end
-  
+
   private
-  
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(
+      :firstname,
+      :lastname,
+      :email, 
+      :password, 
+      :password_confirmation)
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_26_062916) do
+ActiveRecord::Schema.define(version: 2023_07_06_105128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,11 +82,13 @@ ActiveRecord::Schema.define(version: 2021_06_26_062916) do
     t.integer "creator_id"
   end
 
-  create_table "users", id: :serial, force: :cascade do |t|
-    t.text "name", null: false
-    t.text "email", null: false
-    t.text "password", null: false
-    t.index ["email"], name: "users_email_key", unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "firstname"
+    t.string "lastname"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "answers", "choices", name: "answers_choice_id_fkey"
